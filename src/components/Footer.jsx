f798722e211ee2c0f4dcf3b739f535bb87a5fc11
John <antarcticdepths@gmail.com>
2022-10-21 12:40:36 +0200

    center

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,32 +1,30 @@
-import { Grid, Typography } from "@mui/material";
+import { Box, Container, Grid, Typography } from "@mui/material";
 import { styled } from "@mui/material/styles";
 
 export default function Footer() {
   const Footer = styled(Grid)`
-    background: #000;
+    background: #000;git
     color: #fff;
-
     margin-top: 1rem;
-    display: flex;
-    justify-content: center;
-    align-items: center;
   `;
   const Footnote = styled(Typography)`
-    background: #000;
-    color: #fff;
     font-weight: normal;
     letter-spacing: 1px;
-
-    height: 2rem;
-    display: flex;
-    justify-content: center;
-    align-items: center;
   `;
   return (
-    <Footer container>
-      <Grid item>
-        <Footnote variant="subtitle2">App by Munrhalls. 2022.</Footnote>
-      </Grid>
-    </Footer>
+    <Box
+      sx={{
+        width: "100%",
+        background: "#000",
+      }}
+      bgColor="text-secondary"
+      color="white"
+    >
+      <Container justify="center" maxWidth="lg">
+        <Footnote align="center" color="primary" variant="subtitle2">
+          App by Munrhalls. 2022.
+        </Footnote>
+      </Container>
+    </Box>
   );
 }
