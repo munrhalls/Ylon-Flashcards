2f12ed3063e1f58c7c6d122430e6f62fab57140d
John <antarcticdepths@gmail.com>
2022-11-02 08:51:25 +0100

    rework

--- src/MainStories/ShufflingFlashcards/Nav/NavStyle.js
+++ src/MainStories/ShufflingFlashcards/Nav/NavStyle.js
@@ -3,13 +3,6 @@ import { blue } from "@mui/material/colors";
 
 export const useStyles = makeStyles((themes) => {
   return {
-    NavLinksCell: {
-      gridColumnStart: "2",
-      gridColumnEnd: "2",
-      gridRowStart: "3",
-      gridRowEnd: "3",
-      minWidth: "0",
-    },
     btnGroup: {
       width: "100%",
       display: "flex",
